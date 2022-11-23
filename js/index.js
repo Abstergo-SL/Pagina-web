2b9d09ed07a3ff7bd192725784d8b3c60445c507
Erojase <Erojase>
2022-11-23 16:37:15 +0100

    Start tab system

--- js/index.js
+++ js/index.js
@@ -1,5 +1,44 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
+    let result = "nothing"
+
+    const Http = new XMLHttpRequest();
+    const url='http://172.16.51.3:5000/select?table=prueba';
+    Http.open("POST", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+      result = Http.responseText;
+      cositas();
+    }
+
+    function cositas(){
+        console.log(result);
+    }
 
 
+    navEventHandler();
+    
 });
+
+function navEventHandler() {
+  let navitems = document.getElementsByClassName("nav-button");
+
+  for(var element of navitems){
+    element.addEventListener('click', function(event){
+      setActive(navitems, this.innerText);
+      document.getElementById("frame").src = this.innerText + ".html";
+    });
+  }
+}
+
+function setActive(HTMLcollection, content) {
+  for (var elem of HTMLcollection) {
+    if (elem.innerText == content) {
+      elem.classList.toggle("active");
+    }
+    else{
+      elem.classList.remove("active");
+    }
+  }
+}
